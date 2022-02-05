c08f3bebcaf3650b90d52f30b602a1d692fad064
Ragil14 <rahil.gilani@gmail.com>
2022-02-05 14:18:38 -0600

    added path require method

--- develop/routes/html.js
+++ develop/routes/html.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/notes', (req, res) => {
-    res.status(200).send(path.join(__dirname, '../public/notes.html'));
+    res.status(200).sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.status(200).send(path.join(__dirname, '../public/index.html'));
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
