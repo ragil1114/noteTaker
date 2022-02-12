852f0e24445aaf0c84bf3da56fd4c8405bff3e08
Ragil14 <rahil.gilani@gmail.com>
2022-02-12 02:56:11 -0600

    insert comment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const apiRoutes = require('./routes/api');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
